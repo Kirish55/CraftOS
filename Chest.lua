cc99bcfe084f800d495d38a5ff3468d8c3f2e2d5
Kirish55 <kirilltakmokov@mail.ru>
2026-04-23 04:56:42 +0300

    Change chest peripheral type in Chest.lua

--- Chest.lua
+++ Chest.lua
@@ -1,4 +1,4 @@
-local chest = peripheral.find("sophisticatedstorage:netherite_chest")
+local chest = peripheral.find("sophisticatedstorage:chest_0")
 
 for slot, item in pairs(chest.list()) do
     print(slot .. ": " .. item.name .. " x" .. item.count)
