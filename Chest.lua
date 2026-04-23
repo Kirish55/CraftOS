a3b91d0f21376f1b50d05fd76162b4144bbb32fd
Kirish55 <kirilltakmokov@mail.ru>
2026-04-23 05:07:56 +0300

    Update Chest.lua

--- Chest.lua
+++ Chest.lua
@@ -1,4 +1,4 @@
-local chest = peripheral.wrap('sophisticatedstorage:chest_0')
+local chest = peripheral.wrap('computer_0')
 
 for item in pairs(chest.list()) do
     print(item.name .. " x" .. item.count)
